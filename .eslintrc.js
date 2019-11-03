a19e3dff76bd96cbdbdcdd2ad1fd8aec753e9b3c
virb30 <virb30@gmail.com>
2019-11-02 21:55:57 -0300

    Converting Components from Class to Hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: [
-    'airbnb',
-    'prettier',
-    'prettier/react'
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -20,19 +16,15 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier'
-  ],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
-      { extensions: ['jsx', 'js'] }
-    ],
+    'react/jsx-filename-extension': ['warn', { extensions: ['jsx', 'js'] }],
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
     'no-console': ['error', { allow: ['tron'] }],
-    'no-param-reassign': 'off'
+    'no-param-reassign': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
